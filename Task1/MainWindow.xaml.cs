a36b482ff3714222848b37b75233ad3cc558d135
Pavel Tolkach <pb.tolkach@outlook.com>
2022-12-03 20:06:42 +0300

    словарь ресурсов

--- Task1/MainWindow.xaml.cs
+++ Task1/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace Task1
     {
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            string fontName = (sender as ComboBox).SelectedItem as string;
             if (textBox!= null)
                 textBox.FontFamily = new FontFamily(fontName);
 
@@ -32,7 +32,7 @@ namespace Task1
 
         private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
-            string fontSize = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            string fontSize = (sender as ComboBox).SelectedItem as string;
             if (textBox != null)
                 textBox.FontSize = Convert.ToInt32(fontSize);
         }
